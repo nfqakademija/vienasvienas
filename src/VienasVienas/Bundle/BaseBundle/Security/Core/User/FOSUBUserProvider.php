b5c5fbdf340cbb62b4baff0a907f29292e540180
Valdemaras <valdemar.karasevic@gmail.com>
2015-05-17 16:27:18 +0000

    oauth login fix

--- src/VienasVienas/Bundle/BaseBundle/Security/Core/User/FOSUBUserProvider.php
+++ src/VienasVienas/Bundle/BaseBundle/Security/Core/User/FOSUBUserProvider.php
@@ -12,7 +12,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class FOSUBUserProvider extends BaseClass
 {
-
     /**
      * {@inheritdoc}
      */
@@ -24,12 +23,11 @@ class FOSUBUserProvider extends BaseClass
         // On connect - get the access token and the user ID.
         $service = $response->getResourceOwner()->getName();
 
-        $setter = 'set'.ucfirst($service);
-        $setter_id = $setter.'Id';
-        $setter_token = $setter.'AccessToken';
+        $setter = 'set' . ucfirst($service);
+        $setter_id = $setter . 'Id';
+        $setter_token = $setter . 'AccessToken';
 
-        // We "disconnect" previously connected users.
-        if (null !== $previousUser = $this->userManager->findUserBy(
+        if (null !== $previousUser = $this->userManager->findUserByUsernameOrEmail(
                 array
                 (
                     $property => $username
@@ -55,25 +53,19 @@ class FOSUBUserProvider extends BaseClass
     {
         $username = $response->getUsername();
         $email = $response->getEmail();
-        $user = $this->userManager->findUserBy(
-            array
-            (
-                $this->getProperty($response) => $username
-            )
-        );
+        $user = $this->userManager->findUserByEmail($email);
+
+        $service = $response->getResourceOwner()->getName();
+        $setter = 'set' . ucfirst($service);
+        $setter_id = $setter . 'Id';
+        $setter_token = $setter . 'AccessToken';
+
         // When the user is registrating.
         if (null === $user) {
-            $service = $response->getResourceOwner()->getName();
-            $setter = 'set' . ucfirst($service);
-            $setter_id = $setter . 'Id';
-            $setter_token = $setter . 'AccessToken';
             // Create new user here.
             $user = $this->userManager->createUser();
             $user->$setter_id($username);
             $user->$setter_token($response->getAccessToken());
-            // I have set all requested data with the user's username
-            // modify here with relevant data.
-            $user->setUsername($email);
             $user->setEmail($email);
             $user->setPassword($username);
             $user->setEnabled(true);
@@ -84,9 +76,8 @@ class FOSUBUserProvider extends BaseClass
 
         // If user exists - go with the HWIOAuth way.
 
-        $username = $response->getUsername();
 
-        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
+        $user = $this->userManager->findUserByEmail($email);
         if (null === $user || null === $email) {
             throw new AccountNotLinkedException(sprintf("User '%s' not found.", $username));
         }
